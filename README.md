a61b85747c4aa4993318384b2b1d15b20fdf4ab4
Manchitaima4 <121846460+Manchitaima4@users.noreply.github.com>
2025-02-20 01:44:54 -0500

    Se añadieron carpetas, archivo jupyter notebook, archivo app.py, creamos requirement y creamos streamlit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ProyectoSprint7
 Este es el proyecto del sprint 7 de Abraham Imanol Cruz Roncal
+Este proyecto crea un histograma y un diagrama de dispersión con el conjunto de datos vehicles.
+Creamos un repositorio en github, lo clonamos a nuestros repositorios locales y lo modificamos añadiendo una carpeta notebooks con un archivo jupyter, un archivo requirement y un app.py donde se encuentra el codigo general.
---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+hist_button = st.button('Construir histograma')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+#Agrega otro botón que, al hacer clic en él, construya un gráfico de dispersión 
+# plotly-express. 
+# Nuevamente, considera utilizar las funciones st.write() y st.plotly_chart().
+disp_button = st.button("Construir gráfico de dispersión")
+
+if disp_button:
+    # escribimos el mensaje
+    st.write("Creación de un diagrama de dispersión")
+
+    #Crear el diagrama de dispersión
+    fig2 = px.scatter(car_data, x="odometer", y="price")
+
+    #Mostramos el grafico
+    st.plotly_chart(fig2, use_container_width=True)
+
